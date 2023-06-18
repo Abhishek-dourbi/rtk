b691507f3a9f3dd0118109f1f701032545993841
Abhishek-dourbi <abhishekdourbi98@gmail.com>
2023-06-18 21:07:47 +0530

    change to ES6 module imports

---
+++ react-rtk-demo/src/features/icecream/icecreamSlice.js
@@ -0,0 +1,33 @@
+import { ordered as cakeOrdered } from '../cake/cakeSlice';
+
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    numOfIceCreams: 20
+}
+
+const icecreamSlice = createSlice({
+    name: 'icecream',
+    initialState,
+    reducers: {
+        ordered: (state) => {
+            state.numOfIceCreams--;
+        },
+        restocked: (state, action) => {
+            state.numOfIceCreams += action.payload
+        }
+    },
+    // extraReducers: {
+    //     ['cake/ordered']: (state, action) => {
+    //         state.numOfIceCreams--;
+    //     }
+    // }
+    extraReducers: (builder) => {
+        builder.addCase(cakeOrdered, (state) => {
+            state.numOfIceCreams--
+        })
+    }
+})
+
+export default icecreamSlice.reducer;
+export const { ordered, restocked } = icecreamSlice.actions;
