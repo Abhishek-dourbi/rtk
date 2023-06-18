0011c1565624bdb9bceb4065e98febc127cb4385
Abhishek-dourbi <abhishekdourbi98@gmail.com>
2023-06-18 17:25:44 +0530

    add icecream slice for rtk-demo

--- rtk-demo/index.js
+++ rtk-demo/index.js
@@ -1,5 +1,6 @@
 const store = require('./app/store');
 const cakeActions = require('./features/cake/cakeSlice').cakeActions;
+const iceCreamActions = require('./features/icecream/icecreamSlice').iceCreamActions;
 
 console.log('Initial State', store.getState());
 
@@ -10,6 +11,13 @@ const unsubscribe = store.subscribe(() => {
 store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.ordered());
+
 store.dispatch(cakeActions.restocked(3));
+
+store.dispatch(iceCreamActions.ordered());
+store.dispatch(iceCreamActions.ordered());
+store.dispatch(iceCreamActions.ordered());
+
+store.dispatch(iceCreamActions.restocked(3));
 
 unsubscribe();
