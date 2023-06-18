3a9f1e882b2131587bb3d222fbe29eee445f60c0
Abhishek-dourbi <abhishekdourbi98@gmail.com>
2023-06-18 18:08:05 +0530

    handle async actions in rtk-demo

--- rtk-demo/index.js
+++ rtk-demo/index.js
@@ -1,6 +1,7 @@
 const store = require('./app/store');
 const cakeActions = require('./features/cake/cakeSlice').cakeActions;
 const iceCreamActions = require('./features/icecream/icecreamSlice').iceCreamActions;
+const fetchUsers = require('./features/user/userSlice').fetchUsers;
 
 console.log('Initial State', store.getState());
 
@@ -8,16 +9,18 @@ const unsubscribe = store.subscribe(() => {
     console.log('Updated State', store.getState());
 })
 
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.ordered());
+store.dispatch(fetchUsers());
 
-store.dispatch(cakeActions.restocked(3));
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.ordered());
 
-store.dispatch(iceCreamActions.ordered());
-store.dispatch(iceCreamActions.ordered());
-store.dispatch(iceCreamActions.ordered());
+// store.dispatch(cakeActions.restocked(3));
 
-store.dispatch(iceCreamActions.restocked(3));
+// store.dispatch(iceCreamActions.ordered());
+// store.dispatch(iceCreamActions.ordered());
+// store.dispatch(iceCreamActions.ordered());
 
-unsubscribe();
+// store.dispatch(iceCreamActions.restocked(3));
+
+// unsubscribe();
