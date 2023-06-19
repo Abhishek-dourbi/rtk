61efaaff8b051f440a7dd572c55299dbf64d85b1
Abhishek-dourbi <abhishekdourbi98@gmail.com>
2023-06-19 08:12:45 +0530

    add jsx files

---
+++ react-rtk-demo/src/features/icecream/icecreamView.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const IcecreamView = () => {
+  return (
+    <div>
+        <h2>
+            Number of Ice Creams -
+        </h2>
+        <button>
+            Order Ice Cream
+        </button>
+        <button>
+            Restock Ice Creams
+        </button>
+    </div>
+  )
+}
+
+export default IcecreamView;
