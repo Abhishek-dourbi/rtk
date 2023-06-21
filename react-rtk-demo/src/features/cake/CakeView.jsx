3143f96888a37fd5dc8d49218f9b9430bd248cfa
Abhishek-dourbi <abhishekdourbi98@gmail.com>
2023-06-21 08:38:39 +0530

    wire useDispatch hook

--- react-rtk-demo/src/features/cake/CakeView.jsx
+++ react-rtk-demo/src/features/cake/CakeView.jsx
@@ -1,18 +1,24 @@
 import React from 'react'
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { ordered, restocked } from '../../features/cake/cakeSlice';
 
 const CakeView = () => {
   const numOfCakes = useSelector((state) => state.cake.numOfCakes);
+  const dispatch = useDispatch();
 
   return (
     <div>
         <h2>
             Number of Cakes - {numOfCakes}
         </h2>
-        <button>
+        <button onClick={() => {
+          dispatch(ordered())
+        }}>
             Order Cake
         </button>
-        <button>
+        <button onClick={() => {
+          dispatch(restocked(5))
+        }}>
             Restock Cakes
         </button>
     </div>
--- react-rtk-demo/src/features/icecream/icecreamView.jsx
+++ react-rtk-demo/src/features/icecream/icecreamView.jsx
@@ -1,18 +1,27 @@
-import React from 'react'
-import { useSelector } from 'react-redux';
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { ordered, restocked } from '../../features/icecream/icecreamSlice';
 
 const IcecreamView = () => {
-  const numOfIceCreams = useSelector((state) => state.cake.numOfIceCreams);
+  const [value, setValue] = useState(1);
+
+  const numOfIceCreams = useSelector((state) => state.iceCream.numOfIceCreams);
+  const dispatch = useDispatch();
 
   return (
     <div>
         <h2>
             Number of Ice Creams - {numOfIceCreams}
         </h2>
-        <button>
+        <button onClick={() => {
+          dispatch(ordered())
+        }}>
             Order Ice Cream
         </button>
-        <button>
+        <input type='text' value={value} onChange={e => setValue(parseInt(e.target.value))} />
+        <button onClick={() => {
+          dispatch(restocked(value))
+        }}>
             Restock Ice Creams
         </button>
     </div>
